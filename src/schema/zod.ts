be3567a69b885156ba3fd0b30107c29bbdc41107
vitPinchuk <vit.pinchuck@gmail.com>
2025-08-27 20:51:09 +0300

    update zod

--- src/schema/zod.ts
+++ src/schema/zod.ts
@@ -1,5 +1,14 @@
 import { z } from "zod";
 
+export const signInSchema = z.object({
+  email: z.string().min(1, "Email is required").email("Invalid email"),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(6, "Password must be at least 6 characters")
+    .max(32, "Password must be less than 32 characters"),
+});
+
 export const ingredientSchema = z.object({
   name: z.string().min(1, "Name is required"),
   category: z.enum([
@@ -15,6 +24,6 @@ export const ingredientSchema = z.object({
     .number()
     .min(0, { message: "Price must be positive" })
     .nullable()
-    .or(z.nan().transform(() => null)),
+    .or(z.nan().transform(() => null)), // чтобы null/NaN пропускались
   description: z.string().optional(),
 });
